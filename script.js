e667a05943388f35d89509b82cd6ee09eb0bba3c
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-07 12:56:07 +0300

    feat: add square divs to container

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const CONTAINER = '.container';
+const GRID_SIZE = 16 * 16;
 
 const containerElement = document.querySelector(CONTAINER);
+
+for (let i = 0; i < GRID_SIZE; i++) {
+  const squareDiv = document.createElement('div');
+  squareDiv.classList.add('square');
+  containerElement.append(squareDiv);
+}
