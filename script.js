f1833f7f4a686442ead2629007b097bacb401243
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-07 14:51:13 +0300

    feat: add change square opacity on hover

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ const containerElement = document.querySelector(CONTAINER);
 for (let i = 0; i < GRID_SIZE; i++) {
   const squareDiv = document.createElement('div');
   squareDiv.classList.add('square');
+  squareDiv.style.opacity = 1;
   containerElement.append(squareDiv);
 }
+
+/**
+ * 
+ * @param {Event} e 
+ */
+const changeColor = (e) => {
+  const {target} = e;
+
+  target.style.opacity = target.style.opacity - 0.1;
+}
+
+containerElement.addEventListener('mouseover', changeColor);
